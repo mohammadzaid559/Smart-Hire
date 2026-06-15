ad7b0320933eb0f92558ab1abb9355df4bdab0b8
M-Ubaid-Asif <Mohammad Zaid>
2026-06-16 02:31:01 +0530

    dooo

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "mongodb+srv://mohammadzaid559_db_user:zaidSmartHire@cluster0.kvdmizu.mongodb.net/?appName=Cluster0",
 });
 
 export default API;
